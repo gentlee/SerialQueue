aaad211cb77e25ea32c6a52a927d5d5847921fec
Alexander Danilov <d.gentlee@gmail.com>
2016-07-22 01:15:59 +0300

    implement serial queue, add some tests

---
+++ SerialQueue/SerialQueue/SerialQueue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Danilov
+{
+    public class SerialQueue
+    {
+        readonly object _locker = new object();
+        Task _lastTask;
+
+        public Task RunAsync(Action action)
+        {
+            lock (_locker) {
+                _lastTask = _lastTask != null ? _lastTask.ContinueWith(_ => action()) : Task.Run(action);
+                return _lastTask;
+            }
+        }
+
+        public Task<T> RunAsync<T>(Func<T> function)
+        {
+            lock (_locker) {
+                var task = _lastTask != null ? _lastTask.ContinueWith(_ => function()) : Task.Run(function);
+                _lastTask = task;
+                return task;
+            }
+        }
+    }
+}
+
---
+++ SerialQueue/Tests/Test.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+using Danilov;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public async Task TplIsNotFifo()
+        {
+            // Assign
+
+            var list = new List<int>();
+            var tasks = new List<Task>();
+            var range = Enumerable.Range(0, 1000);
+
+            // Act
+
+            foreach (var number in range) {
+                tasks.Add(Task.Factory.StartNew(() => list.Add(number), TaskCreationOptions.PreferFairness));
+            }
+
+            await Task.WhenAll(tasks);
+
+            // Assert
+
+            Assert.False(range.SequenceEqual(list));
+        }
+
+        [Test()]
+        public async Task QueueAction()
+        {
+            // Assign
+
+            var queue = new SerialQueue();
+            var list = new List<int>();
+            var tasks = new List<Task>();
+            var range = Enumerable.Range(0, 1000);
+
+            // Act
+
+            foreach (var number in range) {
+                tasks.Add(queue.RunAsync(() => list.Add(number)));
+            }
+
+            await Task.WhenAll(tasks);
+
+            // Assert
+
+            Assert.True(range.SequenceEqual(list));
+        }
+
+        [Test()]
+        public async Task QueueFunction()
+        {
+            // Assign
+
+            var queue = new SerialQueue();
+            var tasks = new List<Task<int>>();
+            var range = Enumerable.Range(0, 1000);
+
+            // Act
+
+            foreach (var number in range) {
+                tasks.Add(queue.RunAsync(() => number));
+            }
+
+            await Task.WhenAll(tasks);
+
+            // Assert
+
+            Assert.True(range.SequenceEqual(tasks.Select(x => x.Result)));
+        }
+    }
+}
+
